3ad066e0e19a4c8f69ca5a1ef3275a1704df5892
T126075 <t126075@ed.sus.ac.jp>
2026-08-08 05:46:04 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,111 @@
+const menulist = document.querySelector(".menu-list");
+const buttons = document.querySelectorAll(".menu-button");
+
+const menus = [
+  {
+    name: "シュトーレン",
+    category: "limited",
+    img: "シュトーレン.jpg",
+    price: 750,
+  },
+  {
+    name: "パネトーネ",
+    category: "limited",
+    img: "パネトーネ.jpg",
+    price: 550,
+  },
+  {
+    name: "こだわり食パン",
+    category: "bread",
+    img: "こだわり食パン.jpg",
+    price: 400,
+  },
+  {
+    name: "マーブル食パン(苺)",
+    category: "bread",
+    img: "マーブル食パン(苺).jpg",
+    price: 450,
+  },
+  {
+    name: "マーブル食パン(チョコ)",
+    category: "bread",
+    img: "マーブル食パン(チョコ).jpg",
+    price: 450,
+  },
+  {
+    name: "マーブル食パン(抹茶)",
+    category: "bread",
+    img: "マーブル食パン(抹茶).jpg",
+    price: 450,
+  },
+  {
+    name: "マーブル食パン(あんこ)",
+    category: "bread",
+    img: "マーブル食パン(あんこ).jpg",
+    price: 450,
+  },
+  {
+    name: "ハムチーズクロワッサン",
+    category: "savoury",
+    img: "ハムチーズクロワッサン.jpg",
+    price: 350,
+  },
+  {
+    name: "マヨコーンパン",
+    category: "savoury",
+    img: "マヨコーンパン.jpg",
+    price: 300,
+  },
+  {
+    name: "パン",
+    category: "sweet",
+    img: "パン.jpg",
+    price: 450,
+  },
+];
+
+function displayMenu(category) {
+  console.log("選択されたカテゴリー:", category);
+  menulist.innerHTML = "";
+
+  for (let i = 0; i < menus.length; i++) {
+
+    if (category === "all" || menus[i].category === category) {
+
+      const { name, img, price } = menus[i];
+
+      const content = `
+        <div>
+          <img src="img/${img}" alt="">
+          <h2>${name}</h2>
+          <p>${price}円</p>
+        </div>
+      `;
+
+      menulist.insertAdjacentHTML("beforeend", content);
+    }
+  }
+}
+
+const params = new URLSearchParams(window.location.search);
+const category = params.get("category");
+
+if (category) {
+  displayMenu(category);
+} else {
+  displayMenu("all");
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const category = button.dataset.category;
+
+    displayMenu(category);
+
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+    });
+
+    button.classList.add("active");
+  });
+});
